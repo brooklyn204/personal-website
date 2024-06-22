7c0b96f8e4956588de289ed5de1be9797c18b8f3
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2024-06-21 20:37:44 -0700

    Add basic framework with some dummy content

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <div className="block">
+    <div className="block one">
       <div className="left">
         <div className="header">
           <img src={profile} className="profile_pic" alt="Profile picture" />
@@ -27,12 +27,87 @@ function App() {
           </ul>
         </div>
       </div>
+      <div className="center">
+
+      </div>
       <div className="right">
-        <a href="wikipedia.org">GitHub</a>
-        <a href="wikipedia.org">LinkedIn</a>
-        <a href="wikipedia.org">Resume</a>
-        <a href="wikipedia.org">GoodReads</a>
-        <a href="wikipedia.org">Strava</a>
+        <div className="connection"><a href="https://github.com/brooklyn204">GitHub</a></div>
+        <div className="connection"><a href="https://www.linkedin.com/in/brooklyn-cooper-43aba4251/">LinkedIn</a></div>
+        <div className="connection"><a href="BrooklynCooperResume.pdf" download>Resume</a></div>
+        <div className="connection"><a href="https://www.goodreads.com/user/show/135269773-brooklyn">GoodReads</a></div>
+        <div className="connection"><a href="https://www.strava.com/athletes/123785873">Strava</a></div>
+      </div>
+    </div>
+    <div className="block two">
+      <div className="left">
+        <div className="header">
+          <h2>Recent Projects</h2>
+        </div>
+        <div className="body">
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+        </div>
+      </div>
+    </div>
+    <div className="block three">
+      <div className="left">
+        <div className="header">
+          <h2>What I'm listening to</h2>
+        </div>
+        <div className="body">
+          <ul>
+            <li>CS @ UMN</li>
+            <li>Intern @ Microsoft and Garmin</li>
+            <li>Backend dev</li>
+            <li>Cloud solutions enthusiast</li>
+            <li>Cross-disciplinary communicator</li>
+            <li>Runner, bookworm and sci-fi nerd</li>
+          </ul>
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="right">
+        <div className="header">
+          <h2>My runs</h2>
+        </div>
+        <div className="body">
+          <ul>
+            <li>CS @ UMN</li>
+            <li>Intern @ Microsoft and Garmin</li>
+            <li>Backend dev</li>
+            <li>Cloud solutions enthusiast</li>
+            <li>Cross-disciplinary communicator</li>
+            <li>Runner, bookworm and sci-fi nerd</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div className="block four">
+      <div className="left">
+        <div className="header">
+          <h2>Conversation starters and other tidbits</h2>
+        </div>
+        <div className="body">
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+        </div>
+      </div>
+    </div>
+    <div className="block five">
+      <div className="left">
+        <div className="header">
+          <h2>You made it to the end! have a fun game.</h2>
+        </div>
+        <div className="body">
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+          <img src={profile} className="profile_pic" alt="Profile picture" />
+        </div>
       </div>
     </div>
     </>
