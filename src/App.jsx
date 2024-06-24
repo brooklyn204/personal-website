79614db437528c3fac10fdac3910439428371c97
brooklyn204 <brooklyn.cooper.mail@gmail.com>
2024-06-23 19:36:17 -0700

    Add basic Github API functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import profile from './assets/profile.jpg'
+import githubIcon from './assets/github-logo.png'
+import linkedinIcon from './assets/linkedin-logo.png'
+import resumeIcon from './assets/resume-logo.png'
+import goodreadsIcon from './assets/goodreads-logo.png'
+import stravaIcon from './assets/strava-logo.png'
 import './App.css'
 
+function repoCard(name, modified, collaborators, url) {
+  this.name = name;
+  this.modified = modified;
+  this.collaborators = collaborators;
+  this.url = url;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/brooklyn204/repos", {
+      "headers": {
+          'X-GitHub-Api-Version': '2022-11-28'
+      },
+      "method": "GET"
+    }).then(response => response.json())
+    .then(repos => {
+      console.log(repos);
+      var cards = [];
+      for (var i in repos) {
+        let card = new repoCard(repos[i].name,repos[i].updated_at,null,repos[i].html_url);
+        cards.push(card);
+      } 
+      setRepos(cards)});
+   }, []);
 
   return (
     <>
@@ -18,7 +49,7 @@ function App() {
         </div>
         <div className="body">
           <ul>
-            <li>CS @ UMN</li>
+            <li>CS Student @ UMN</li>
             <li>Intern @ Microsoft and Garmin</li>
             <li>Backend dev</li>
             <li>Cloud solutions enthusiast</li>
@@ -31,23 +62,42 @@ function App() {
 
       </div>
       <div className="right">
-        <div className="connection"><a href="https://github.com/brooklyn204">GitHub</a></div>
-        <div className="connection"><a href="https://www.linkedin.com/in/brooklyn-cooper-43aba4251/">LinkedIn</a></div>
-        <div className="connection"><a href="BrooklynCooperResume.pdf" download>Resume</a></div>
-        <div className="connection"><a href="https://www.goodreads.com/user/show/135269773-brooklyn">GoodReads</a></div>
-        <div className="connection"><a href="https://www.strava.com/athletes/123785873">Strava</a></div>
+        <a className="connection" href="https://github.com/brooklyn204">
+          <img src={githubIcon} className="icon" alt="Github icon"/>
+          <h3>GitHub</h3>
+        </a>
+        <a className="connection" href="https://www.linkedin.com/in/brooklyn-cooper-43aba4251/">
+          <img src={linkedinIcon} className="icon" alt="LinkedIn icon"/>
+          <h3>LinkedIn</h3>
+        </a>
+        <a className="connection" href="BrooklynCooperResume.pdf" download>
+          <img src={resumeIcon} className="icon" alt="Resume icon"/>
+          <h3>Resume</h3>
+        </a>
+        <a className="connection" href="https://www.goodreads.com/user/show/135269773-brooklyn">
+          <img src={goodreadsIcon} className="icon" alt="Goodreads icon"/>
+          <h3>GoodReads</h3>
+        </a>
+        <a className="connection" href="https://www.strava.com/athletes/123785873">
+          <img src={stravaIcon} className="icon" alt="Strava icon"/>
+          <h3>Strava</h3>
+          </a>
       </div>
     </div>
-    <div className="block two">
-      <div className="left">
+    <div className="block">
+      <div className="two">
         <div className="header">
           <h2>Recent Projects</h2>
         </div>
-        <div className="body">
-          <img src={profile} className="profile_pic" alt="Profile picture" />
-          <img src={profile} className="profile_pic" alt="Profile picture" />
-          <img src={profile} className="profile_pic" alt="Profile picture" />
-          <img src={profile} className="profile_pic" alt="Profile picture" />
+        <div className="scroll">
+          {repos.map((card) => {
+            return (
+              <a className="repo" href={card.url}>
+                <h3>{card.name}</h3>
+                <br/>
+                <p className="info">{card.modified}</p>
+              </a>)
+          })}
         </div>
       </div>
     </div>
